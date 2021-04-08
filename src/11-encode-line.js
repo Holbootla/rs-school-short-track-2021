307c5d3c7dbb4bf4c88f51790a7e11cad1fc2bca
Nickita Lashch <holbootla@gmail.com>
2021-04-08 21:16:36 +0300

    feat: finish task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  const arr = str.split('');
+  let count = 1;
+  let letter = arr[0];
+  while (arr.length > 0) {
+    if (arr[0] === arr[1]) {
+      count += 1;
+      arr.shift();
+    } else {
+      if (count !== 1) {
+        result.push(`${count}${letter}`);
+      } else {
+        result.push(`${letter}`);
+      }
+      arr.shift();
+      count = 1;
+      letter = arr[0] || null;
+    }
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
