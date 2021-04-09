6a424b9b65588daa456f14a97a14862446ceceb3
Nickita Lashch <holbootla@gmail.com>
2021-04-09 20:34:16 +0300

    feat: finish task 15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,11 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  // const arr = email.split('');
+  // return arr.slice(arr.indexOf('@') + 1, arr.length).join('');
+  const arr = email.split('@');
+  return arr.pop();
 }
 
 module.exports = getEmailDomain;
